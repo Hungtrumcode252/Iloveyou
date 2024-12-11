0bc7c96c88d79476bf27498cf718120336169bb1
Hungtrumcode252 <phamduchung123zzz@gmail.com>
2024-12-11 23:08:08 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Bạn có muốn ăn cứt ko!",
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Bạn có muốn ăn cứt ko?",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  buttonYes: "Có chứ",
+  buttonNo: "Không",
+  titleModar: "Biết mà:))",
   descriptionModar:
-    "Tớ biết là cậu sẽ trả lời vậy mà:))",
+    "",
 };
